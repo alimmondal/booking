4740ebeff75aaf34583b3e7534db6804b7af4303
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-05-22 18:21:29 +0600

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import Navbar from '../../components/navbar/Navbar';
+import './home.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer"></div>
+    </div>
+  );
+}
